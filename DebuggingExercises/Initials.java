4b4c02135fb87f9763e0abab8364e37a2e6306c0
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-08-14 19:57:34 +1000

    Initials completed

---
+++ DebuggingExercises/Initials.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jc319816 on 14/08/16.
+ */
+public class Initials {
+
+    public static void main(String[] args)
+    {
+        char initial_1 = 'J';
+        char initial_2 = 'M';
+        char initial_3 = 'N';
+
+        System.out.println(initial_1 + "." + initial_2 + "." + initial_3 + ".");
+    }
+}
